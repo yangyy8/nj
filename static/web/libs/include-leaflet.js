77882bf939a5b29963a52605fafd9a05ea7c1d86
yangyy8 <yangsnow01@163.com>
2019-08-05 15:09:35 +0800

    yyy

--- static/web/libs/include-leaflet.js
+++ static/web/libs/include-leaflet.js
@@ -36,81 +36,81 @@
         var includes = (targetScript.getAttribute('include') || "").split(",");
         var excludes = (targetScript.getAttribute('exclude') || "").split(",");
         if (!inArray(excludes, 'jquery')) {
-            inputScript("../dist/static/dist/jquery-1.10.2.js");
+            inputScript("../static/dist/jquery-1.10.2.js");
         }
         if (!inArray(excludes, 'leaflet')) {
-            inputCSS("../dist/static/web/libs/leaflet/leaflet.css");
-            inputScript("../dist/static/web/libs/leaflet/leaflet.js");
+            inputCSS("../static/web/libs/leaflet/leaflet.css");
+            inputScript("../static/web/libs/leaflet/leaflet.js");
         }
         if (!inArray(excludes, 'moment')) {
-            inputScript("../dist/static/web/libs/moment/moment.min.js");
-            inputScript("../dist/static/web/libs/moment/zh-cn.js");
+            inputScript("../static/web/libs/moment/moment.min.js");
+            inputScript("../static/web/libs/moment/zh-cn.js");
         }
         if (!inArray(excludes, 'bootstrap')) {
-            inputCSS("../dist/static/web/libs/bootstrap/bootstrap.min.css");
-            inputScript("../dist/static/web/libs/bootstrap/bootstrap.min.js");
+            inputCSS("../static/web/libs/bootstrap/bootstrap.min.css");
+            inputScript("../static/web/libs/bootstrap/bootstrap.min.js");
         }
         if (!inArray(excludes, 'bootstrap-datetimepicker')) {
-            inputCSS("../dist/static/web/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.min.css");
-            inputScript("../dist/static/web/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.min.js");
+            inputCSS("../static/web/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.min.css");
+            inputScript("../static/web/libs/bootstrap-datetimepicker/bootstrap-datetimepicker.min.js");
         }
         if (inArray(includes, 'mapv')) {
-            inputScript("../dist/static/web/libs/mapv/mapv.min.js");
+            inputScript("../static/web/libs/mapv/mapv.min.js");
         }
         if (inArray(includes, 'turf')) {
-            inputScript("../dist/static/web/libs/turf/turf.min.js");
+            inputScript("../static/web/libs/turf/turf.min.js");
         }
         if (inArray(includes, 'echarts')) {
 
-            inputScript("../dist/static/web/libs/echarts/echarts.min.js");
+            inputScript("../static/web/libs/echarts/echarts.min.js");
         }
         if (inArray(includes, 'd3')) {
-            inputScript("../dist/static/web/libs/d3/d3.min.js");
+            inputScript("../static/web/libs/d3/d3.min.js");
         }
         if (inArray(includes, 'd3-hexbin')) {
-            inputScript("../dist/static/web/libs/d3/d3-hexbin.v0.2.min.js");
+            inputScript("../static/web/libs/d3/d3-hexbin.v0.2.min.js");
         }
         if (inArray(includes, 'd3Layer')) {
-            inputScript("../dist/static/web/libs/leaflet/plugins/leaflet.d3Layer/leaflet-d3Layer.min.js");
+            inputScript("../static/web/libs/leaflet/plugins/leaflet.d3Layer/leaflet-d3Layer.min.js");
         }
         if (inArray(includes, 'elasticsearch')) {
-            inputScript("../dist/static/web/libs/elasticsearch/elasticsearch.min.js");
+            inputScript("../static/web/libs/elasticsearch/elasticsearch.min.js");
         }
         if (!inArray(excludes, 'iclient9-leaflet')) {
-            inputScript("../dist/static/dist/iclient9-leaflet.min.js");
+            inputScript("../static/dist/iclient9-leaflet.min.js");
         }
         if (inArray(includes, 'iclient9-leaflet-css')) {
-            inputCSS("../dist/static/dist/iclient9-leaflet.min.css");
+            inputCSS("../static/dist/iclient9-leaflet.min.css");
         }
         if (inArray(includes, 'leaflet.heat')) {
-            inputScript("../dist/static/web/libs/leaflet/plugins/leaflet.heat/leaflet-heat.js");
+            inputScript("../static/web/libs/leaflet/plugins/leaflet.heat/leaflet-heat.js");
         }
         if (inArray(includes, 'osmbuildings')) {
-            inputScript("../dist/static/web/libs/osmbuildings/OSMBuildings-Leaflet.js");
+            inputScript("../static/web/libs/osmbuildings/OSMBuildings-Leaflet.js");
         }
         if (inArray(includes, 'leaflet.markercluster')) {
-            inputCSS("../dist/static/web/libs/leaflet/plugins/leaflet.markercluster/MarkerCluster.Default.css");
-            inputCSS("../dist/static/web/libs/leaflet/plugins/leaflet.markercluster/MarkerCluster.css");
-            inputScript("../dist/static/web/libs/leaflet/plugins/leaflet.markercluster/leaflet.markercluster.js");
+            inputCSS("../static/web/libs/leaflet/plugins/leaflet.markercluster/MarkerCluster.Default.css");
+            inputCSS("../static/web/libs/leaflet/plugins/leaflet.markercluster/MarkerCluster.css");
+            inputScript("../static/web/libs/leaflet/plugins/leaflet.markercluster/leaflet.markercluster.js");
         }
         if (inArray(includes, 'leaflet-icon-pulse')) {
-            inputCSS("../dist/static/web/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.css");
-            inputScript("../dist/static/web/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.js");
+            inputCSS("../static/web/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.css");
+            inputScript("../static/web/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.js");
         }
         if (inArray(includes, 'leaflet.draw')) {
-            inputCSS("../dist/static/web/libs/leaflet/plugins/leaflet.draw/leaflet.draw.css");
-            inputScript("../dist/static/web/libs/leaflet/plugins/leaflet.draw/leaflet.draw.js");
+            inputCSS("../static/web/libs/leaflet/plugins/leaflet.draw/leaflet.draw.css");
+            inputScript("../static/web/libs/leaflet/plugins/leaflet.draw/leaflet.draw.js");
         }
         if (inArray(includes, 'leaflet.pm')) {
-            inputCSS("../dist/static/web/libs/leaflet/plugins/leaflet.pm/leaflet.pm.min.css");
-            inputScript("../dist/static/web/libs/leaflet/plugins/leaflet.pm/leaflet.pm.min.js");
+            inputCSS("../static/web/libs/leaflet/plugins/leaflet.pm/leaflet.pm.min.css");
+            inputScript("../static/web/libs/leaflet/plugins/leaflet.pm/leaflet.pm.min.js");
         }
         if (inArray(includes, 'leaflet.miniMap')) {
-            inputCSS("../dist/static/web/libs/leaflet/plugins/leaflet-miniMap/dist/Control.MiniMap.min.css");
-            inputScript("../dist/static/web/libs/leaflet/plugins/leaflet-miniMap/dist/Control.MiniMap.min.js");
+            inputCSS("../static/web/libs/leaflet/plugins/leaflet-miniMap/dist/Control.MiniMap.min.css");
+            inputScript("../static/web/libs/leaflet/plugins/leaflet-miniMap/dist/Control.MiniMap.min.js");
         }
         if (inArray(includes, 'leaflet.sidebyside')) {
-            inputScript("../dist/static/web/libs/leaflet/plugins/leaflet-side-by-side/leaflet-side-by-side.min.js");
+            inputScript("../static/web/libs/leaflet/plugins/leaflet-side-by-side/leaflet-side-by-side.min.js");
         }
     }
 
